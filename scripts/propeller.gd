18c53e8fb0cb16ae6837e6d3ab69cd71f64602ee
TimonDeBeste <timon.schol@gmail.com>
2026-06-22 00:02:54 +0200

    Change mechanic

--- scripts/propeller.gd
+++ scripts/propeller.gd
@@ -1,23 +1,25 @@
 extends Node2D
 
 
-@export var raycast: RayCast2D
+@export var raycasts: Array[RayCast2D]
+
 @export var exceptions: Array[CollisionObject2D]
 
 func _ready() -> void:
-	for exception in exceptions:
-		raycast.add_exception(exception)
+	for raycast in raycasts:
+		for exception in exceptions:
+			raycast.add_exception(exception)
 	$AnimatedSprite2D.play("default")
 
 
 
 func _process(_delta: float) -> void:
-	
-	if raycast.is_colliding(): # Propeller system
-		var collider = raycast.get_collider()
-		if collider.is_class("RigidBody2D"):
-			var distance = Vector2(position.x - collider.position.x, position.y - collider.position.y)
-			var force = Vector2(-distance.x, (raycast.target_position.y + distance.y))
-			collider.apply_force(force)
+	for raycast in raycasts:# Propeller system
+		if raycast.is_colliding(): 
+			var collider = raycast.get_collider()
+			if collider.is_class("RigidBody2D"):
+				var distance = Vector2(position.x - collider.position.x, position.y - collider.position.y)
+				var force = Vector2(-distance.x, (raycast.target_position.y + distance.y))
+				collider.apply_force(force)
 			
 	position.x = get_global_mouse_position().x
