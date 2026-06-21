5820a19577ba738fde2086ad9800e3f8294721d4
TimonDeBeste <timon.schol@gmail.com>
2026-06-21 15:28:40 +0200

    Tweak numbers, format code

--- scripts/propeller.gd
+++ scripts/propeller.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
+
 @export var raycast: RayCast2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if raycast.is_colliding():
+	
+	if raycast.is_colliding(): # Propeller system
 		var collider = raycast.get_collider()
 		if collider.is_class("RigidBody2D"):
 			var distance = Vector2(position.x - collider.position.x, position.y - collider.position.y)
-			var force = Vector2(-distance.x, 32 * (raycast.target_position.y + distance.y))
+			var force = Vector2(-distance.x * 16, 8 * (raycast.target_position.y + distance.y))
 			collider.apply_force(force)
+			
+	position.x = get_global_mouse_position().x
